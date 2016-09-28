ae2b764377cdce27bba7f9374619a4fd6203cdf9
Jose Ferrús <jose.ferrus.94@gmail.com>
2016-09-28 17:10:11 +0200

    Update BufferSemaforos.java

--- Semaforos/src/ejercicio3p6lpsemaforos/BufferSemaforos.java
+++ Semaforos/src/ejercicio3p6lpsemaforos/BufferSemaforos.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio3p6lpsemaforos;
 
 import java.util.concurrent.Semaphore;
 
 /**
  *
- * @author Ricard
+ * @author Ricard y Jose
  */
 public class BufferSemaforos {
     int [] buffer;
