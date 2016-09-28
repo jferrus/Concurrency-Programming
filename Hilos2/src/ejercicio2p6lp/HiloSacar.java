e3aef481e696b3ca77f718079b48bdb93fdc1963
Jose Ferrús <jose.ferrus.94@gmail.com>
2016-09-28 17:08:18 +0200

    Update HiloSacar.java

--- Hilos2/src/ejercicio2p6lp/HiloSacar.java
+++ Hilos2/src/ejercicio2p6lp/HiloSacar.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio2p6lp;
 
 import java.util.logging.Level;
@@ -10,7 +5,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author Ricard
+ * @author Ricard y Jose
  */
 public class HiloSacar extends Thread{
     int cantidad = 0;
