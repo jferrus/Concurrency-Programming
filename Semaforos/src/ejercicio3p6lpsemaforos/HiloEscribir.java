a220cea841834baf7d5964da3b8d6cff16df0f9e
Jose Ferrús <jose.ferrus.94@gmail.com>
2016-09-28 17:10:35 +0200

    Update HiloEscribir.java

--- Semaforos/src/ejercicio3p6lpsemaforos/HiloEscribir.java
+++ Semaforos/src/ejercicio3p6lpsemaforos/HiloEscribir.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio3p6lpsemaforos;
 
 /**
  *
- * @author Ricard
+ * @author Ricard y Jose
  */
 public class HiloEscribir extends Thread{
     BufferSemaforos buffer;
