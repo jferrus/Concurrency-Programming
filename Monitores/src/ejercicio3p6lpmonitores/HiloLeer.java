69b89c884c71b1ce185e442abf39e6b581f9f0e7
Jose Ferrús <jose.ferrus.94@gmail.com>
2016-09-28 17:09:45 +0200

    Update HiloLeer.java

--- Monitores/src/ejercicio3p6lpmonitores/HiloLeer.java
+++ Monitores/src/ejercicio3p6lpmonitores/HiloLeer.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio3p6lpmonitores;
 
 import static java.lang.Thread.sleep;
 
 /**
  *
- * @author Ricard
+ * @author Ricard y Jose
  */
 public class HiloLeer extends Thread{
     BufferMonitores buffer;
